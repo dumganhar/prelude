62b4309f04059b9727164e45e26aedf8d6cd3041
James Chen <jianhua.chen@cocos2d-x.org>
2013-04-04 13:05:57 +0800

    global whitespace mode

--- personal/main.el
+++ personal/main.el
@@ -1,6 +1,6 @@
 (desktop-save-mode 1) ;自动打开上次文件
 
-(scroll-bar-mode -1);去掉滚动条 
+(scroll-bar-mode -1);去掉滚动条
 (disable-theme 'zenburn)
 ;; color theme
 (load-file "~/.emacs.d/themes/color-theme-almost-monokai.el")
@@ -11,6 +11,8 @@
 
 (require 'linum)
 (global-linum-mode)
+
+(global-whitespace-mode 1)
 
 ;; Comment and Uncomment
 (defun comment-or-uncomment-line-or-region ()
@@ -67,39 +69,39 @@
       nil t))
 
 (defun qiang-make-font-string (font-name font-size)
-  (if (and (stringp font-size) 
+  (if (and (stringp font-size)
            (equal ":" (string (elt font-size 0))))
       (format "%s%s" font-name font-size)
     (format "%s %s" font-name font-size)))
 
 (defun qiang-set-font (english-fonts
-					   english-font-size
-					   chinese-fonts
-					   &optional chinese-font-size)
+                       english-font-size
+                       chinese-fonts
+                       &optional chinese-font-size)
   "english-font-size could be set to \":pixelsize=18\" or a integer.
 If set/leave chinese-font-size to nil, it will follow english-font-size"
-  (require 'cl)						 ; for find if
+  (require 'cl); for find if
   (let ((en-font (qiang-make-font-string
-				  (find-if #'qiang-font-existsp english-fonts)
-				  english-font-size))
-		(zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts)
-							:size chinese-font-size)))
-	;; Set the default English font
-	;;
-	;; The following 2 method cannot make the font settig work in new frames.
-	;; (set-default-font "Consolas:pixelsize=18")
-	;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
-	;; We have to use set-face-attribute
-	(message "Set English Font to %s" en-font)
-	(set-face-attribute
-	 'default nil :font en-font)
-	;; Set Chinese font
-	;; Do not use 'unicode charset, it will cause the english font setting invalid
-	(message "Set Chinese Font to %s" zh-font)
-	(dolist (charset '(kana han symbol cjk-misc bopomofo))
-	  (set-fontset-font (frame-parameter nil 'font)
-						charset
-						zh-font))))
+                  (find-if #'qiang-font-existsp english-fonts)
+                  english-font-size))
+        (zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts)
+                            :size chinese-font-size)))
+    ;; Set the default English font
+    ;;
+    ;; The following 2 method cannot make the font settig work in new frames.
+    ;; (set-default-font "Consolas:pixelsize=18")
+    ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
+    ;; We have to use set-face-attribute
+    (message "Set English Font to %s" en-font)
+    (set-face-attribute
+     'default nil :font en-font)
+    ;; Set Chinese font
+    ;; Do not use 'unicode charset, it will cause the english font setting invalid
+    (message "Set Chinese Font to %s" zh-font)
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+      (set-fontset-font (frame-parameter nil 'font)
+                        charset
+                        zh-font))))
 
 (qiang-set-font
  '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New") ":pixelsize=14"
