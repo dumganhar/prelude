395239b245dfebd9a56f97e412a56d2379f67009
James Chen <jianhua.chen@cocos2d-x.org>
2013-04-04 09:45:38 +0800

    Updating my-customize-function.el.

--- personal/my-customize-function.el
+++ personal/my-customize-function.el
@@ -10,16 +10,16 @@
   (while (search-forward-regexp "\\([^{]\\)}" nil t) (replace-match "\\1end" t nil))
 
   (goto-char 1)
-  (while (search-forward-regexp "^[A-z:]+[\s\t]*[A-z0-9_]*::\\(.*\\)" nil t) (replace-match "function \\1" t nil))
+  (while (search-forward-regexp "^[A-z:]+[\s\t]*[A-z0-9_]*::\\(.*\\)" nil t) (replace-match "local function \\1" t nil))
 
   (goto-char 1)
-  (while (search-forward-regexp "\\(CCSize\\|CCRect\\|CCPoint\\)[\s\t]*\\([A-z_0-9]+\\)" nil t) (replace-match "local \\2" t nil))
+  (while (search-forward-regexp "\\(CCSize\\|CCRect\\|CCPoint\\)[\s\t]+\\([A-z_0-9]+\\)" nil t) (replace-match "local \\2" t nil))
 
   (goto-char 1)
   (while (search-forward-regexp "[Cc]\\{2\\}[Ll][Oo][Gg]" nil t) (replace-match "cclog" t nil))
 
   (goto-char 1)
-  (while (search-forward-regexp "\\([0-9]*\\)\\.\\([0-9]*\\)f" nil t) (replace-match "\\1.\\2" t nil))
+  (while (search-forward-regexp "\\([0-9]*\\)\\.\\([0-9]*\\)f\\([^A-z]\\)" nil t) (replace-match "\\1.\\2\\3" t nil))
   
   (goto-char 1)
   (while (search-forward-regexp "CC[A-z0-9_]+.*\\*\\(.*=\\)" nil t) (replace-match "local \\1" t nil))
@@ -42,6 +42,9 @@
   
   (goto-char 1)
   (while (search-forward "CC_UNUSED" nil t) (replace-match "" nil t))
+
+  (goto-char 1)
+  (while (search-forward "" nil t) (replace-match "" nil t))
 
   )
 
