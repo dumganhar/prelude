bb6a36313473802a82282e2907ee3524b2861007
James Chen <jianhua.chen@cocos2d-x.org>
2014-03-23 22:04:39 +0800

    iTerm emacs config

--- core/prelude-osx.el
+++ core/prelude-osx.el
@@ -34,9 +34,9 @@
 
 ;; On OS X Emacs doesn't use the shell PATH if it's not started from
 ;; the shell. Let's fix that:
-(prelude-require-packages '(exec-path-from-shell))
-(require 'exec-path-from-shell)
-(exec-path-from-shell-initialize)
+; (prelude-require-packages '(exec-path-from-shell))
+; (require 'exec-path-from-shell)
+; (exec-path-from-shell-initialize)
 
 ;; It's all in the Meta
 (setq ns-function-modifier 'hyper)
--- personal/main.el
+++ personal/main.el
@@ -1,7 +1,7 @@
 (desktop-save-mode 1) ;自动打开上次文件
 
-(scroll-bar-mode -1);去掉滚动条
-; (disable-theme 'zenburn)
+; (scroll-bar-mode -1);去掉滚动条
+(disable-theme 'zenburn)
 ; ;; color theme
 ; (load-file "~/.emacs.d/themes/color-theme-almost-monokai.el")
 ; (color-theme-almost-monokai)
@@ -20,7 +20,15 @@
 
 (global-whitespace-mode 1)
 
-(prelude-swap-meta-and-super)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-c C-c") 'mc/edit-lines)
+(global-set-key (kbd "C-c C-n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-p") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-a") 'mc/mark-all-like-this)
 
 ;; Comment and Uncomment
 (defun comment-or-uncomment-line-or-region ()
@@ -72,52 +80,6 @@
 (defun lua-abbrev-mode-off () (abbrev-mode 0))
 (add-hook 'lua-mode-hook 'lua-abbrev-mode-off)
 
-(defun qiang-font-existsp (font)
-  (if (null (x-list-fonts font))
-      nil t))
-
-(defun qiang-make-font-string (font-name font-size)
-  (if (and (stringp font-size)
-           (equal ":" (string (elt font-size 0))))
-      (format "%s%s" font-name font-size)
-    (format "%s-%s" font-name font-size)))
-
-(defun qiang-set-font (english-fonts
-                       english-font-size
-                       chinese-fonts)
-  "english-font-size could be set to \":pixelsize=18\" or a integer.
-If set/leave chinese-font-size to nil, it will follow english-font-size"
-  (require 'cl)                         ; for find if
-  (let ((en-font (qiang-make-font-string
-                  (find-if #'qiang-font-existsp english-fonts)
-                  english-font-size))
-        (zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts))))
-
-    ;; Set the default English font
-    ;;
-    ;; The following 2 method cannot make the font settig work in new frames.
-    ;; (set-default-font "Consolas:pixelsize=18")
-    ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
-    ;; We have to use set-face-attribute
-    (message "Set English Font to %s" en-font)
-    (set-face-attribute
-     'default nil :font en-font)
-
-    ;; Set Chinese font
-    ;; Do not use 'unicode charset, it will cause the english font setting invalid
-    (message "Set Chinese Font to %s" zh-font)
-    (dolist (charset '(kana han symbol cjk-misc bopomofo))
-      (set-fontset-font t
-                        charset
-                        zh-font))))
-
-(setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Zen Hei" . 1.2)))
-(qiang-set-font
- '("Monaco" "Consolas" "DejaVu Sans Mono" "Monospace" "Courier New") 12
- '("WenQuanyi Micro Hei Mono" "SimSun" "Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体") )
-
-
-(set-face-attribute 'default nil :font (font-spec))
 
 ; here are 20 hanzi and 40 english chars, see if they are the same width
 ; 你你你你你你你你你你你你你你你你你你你你
@@ -125,4 +87,7 @@ If set/leave chinese-font-size to nil, it will follow english-font-size"
 
 ;; c c++ 配置
 (require 'init-cc-mode)
+
+
+
 
